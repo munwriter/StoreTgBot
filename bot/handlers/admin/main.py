936d53579349904acdb30e83245e07d0d3de38cf
munwriter <glebvysokov3@gmail.com>
2023-08-11 17:10:12 +0500

    added concealment of classified information

--- bot/handlers/admin/main.py
+++ bot/handlers/admin/main.py
@@ -1,14 +1,23 @@
 from aiogram import Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup
+from aiogram import types
+from dotenv import load_dotenv
+import os
 from bot import keyboards as kb
 from bot.templates import descriptions as dc
-from aiogram import types
+
+
 
 
 def register_admin_handlers(dp: Dispatcher):
+    load_dotenv()
+
     @dp.message_handler(text='Администрирование')
     async def admin_start(message: types.Message):
-        if message.from_user.id == 644920251:
+        if str(message.from_user.id) == os.getenv('ADMIN_ID'):
             await message.answer('Вы вошли в панель администрирования', reply_markup=kb.admin_menu_keyboard)
-        else:
-            await message.answer('Вы не являетесь администратором')
+    
+    
+        
     
--- bot/handlers/other.py
+++ bot/handlers/other.py
@@ -1,14 +1,16 @@
 from aiogram import Dispatcher, types
-from bot import keyboards as kb
 import aiogram.utils.markdown as fmt
-
+from dotenv import load_dotenv
+import os
+from bot import keyboards as kb
 
 def register_other_handlers(dp: Dispatcher):
+    load_dotenv()
 
     @dp.message_handler(text='🧨Начать🧨')
     async def main_menu(message: types.Message):
         await message.answer_sticker('CAACAgQAAxkBAAEJ951k057Igh_S_q3xF3aJhjQwYvWeYgACUQADg2rQEPS6m0vsIMEvMAQ')
-        if message.from_user.id == 644920251:
+        if str(message.from_user.id) == os.getenv('ADMIN_ID'):
             await message.answer(fmt.text('Вы вошли в главное меню\n', fmt.hbold('Administrator mode on')), parse_mode='HTML', reply_markup=kb.main_menu_admin_keyboard)
         else:
             await message.answer('Вы вошли в главное меню', reply_markup=kb.main_menu_keyboard)
